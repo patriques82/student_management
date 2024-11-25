28309e562b325426b35da48b26e3935db3b61ac9
Patrik Nygren <keyturnlogic@gmail.com>
2024-11-25 15:52:51 +0100

    important change

--- students_management_test.py
+++ students_management_test.py
@@ -3,8 +3,8 @@ import unittest
 
 class StudentManagementTest(unittest.TestCase):
     def test_add_student_to_empty_list(self):
-        result = students_management.add([], 7, "Rufus")
-        self.assertEqual(result, [{ "id": 7, "name": "Rufus" }])
+        res = students_management.add([], 7, "Rufus")
+        self.assertEqual(res, [{ "id": 7, "name": "Rufus" }])
 
 if __name__ == '__main__':
     unittest.main()
