03e013b885b5e3eccaa539eab2c4dc83d242c8a8
Patrik Nygren <keyturnlogic@gmail.com>
2024-11-27 10:26:25 +0100

    Added display student funktionality

--- students_management.py
+++ students_management.py
@@ -5,18 +5,34 @@ def add(students, id, name):
     return students
 
 def addStudent():
-    id = input("Whats the id")
-    name = input("Whats the name")
+    id = input("Whats the id\n")
+    name = input("Whats the name\n")
     add(students, id, name)
 
+def display(students, id):
+    for s in students:
+        if s["id"] == id:
+            return s
+
+def displayStudent():
+    id = input("Whats the id\n")
+    student = display(students, id)
+    print(student)
+
 def main():
-    choice = int(input("""
-        1. Add student
-        2. Remove student
-    """))
-    if (choice == 1):
-        addStudent()
-    else:
-        pass
+    while True:
+        choice = int(input("""
+            1. Add student
+            2. Display student
+            3. Save and Quit\n               
+        """))
+        if (choice == 1):
+            addStudent()
+        elif (choice == 2):
+            displayStudent()
+        else:
+            break
+
+# main()
 
 
