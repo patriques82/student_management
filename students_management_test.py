f000f3d92474752df2addeb4ff648355e6e7af03
Patrik Nygren <keyturnlogic@gmail.com>
2024-11-27 10:26:55 +0100

    Added test for display student funktionality

--- students_management_test.py
+++ students_management_test.py
@@ -6,6 +6,12 @@ class StudentManagementTest(unittest.TestCase):
         res = students_management.add([], 7, "Rufus")
         self.assertEqual(res, [{ "id": 7, "name": "Rufus" }])
 
+    def test_display_student(self):
+        student = students_management.display([
+            { "id": 7, "name": "Rufus" }, 
+            { "id": 8, "name": "Charles" }
+        ], 7)
+        self.assertEqual(student, { "id": 7, "name": "Rufus" })
+
 if __name__ == '__main__':
     unittest.main()
-
